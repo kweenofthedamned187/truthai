8b3d18bc2095d1432401971ce85d6f5d1df69af0
robin elaine <robinelainepsrc@aol.com>
2026-07-28 19:13:58 -0400

    chore: add user CRUD helpers

---
+++ backend/app/db/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+
+from app.db import models
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, email: str, hashed_password: str):
+    user = models.User(email=email, hashed_password=hashed_password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
